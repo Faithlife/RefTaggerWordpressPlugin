b4d7d1e1854c719788377c765fcd84c401e826c0
Thomas Beirne <thomas.beirne@logos.com>
2014-02-03 10:58:39 -0800

    Bump version to 2.0.1 for chapter tagging changes.

--- RefTagger.php
+++ RefTagger.php
@@ -4,7 +4,7 @@ Plugin Name: RefTagger
 Plugin URI: http://www.logos.com/reftagger
 Description: Transform Bible references into links to the full text of the verse.
 Author: Logos Bible Software
-Version: 2.0
+Version: 2.0.1
 Author URI: http://www.logos.com/
 */
 
