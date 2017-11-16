361d34e8def37cd21acdcf203cb0316ea1a3413b
Lawrence Rafferty <lawrence.rafferty@gmail.com>
2017-11-15 16:43:48 -0800

    Bump the minor version number to reflect the new Bible choices.

--- RefTagger.php
+++ RefTagger.php
@@ -4,7 +4,7 @@ Plugin Name: RefTagger
 Plugin URI: http://www.logos.com/reftagger
 Description: Transform Bible references into links to the full text of the verse.
 Author: Logos Bible Software
-Version: 2.0.4
+Version: 2.1.0
 Author URI: http://www.logos.com/
 */
 
