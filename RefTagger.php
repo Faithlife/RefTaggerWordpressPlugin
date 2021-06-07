2182221f389f5d708de06373d356e65966687d93
Ronny Woods <39571660+ronnywoods@users.noreply.github.com>
2021-06-07 16:22:26 -0700

    Update version to 2.4.2 in header

--- RefTagger.php
+++ RefTagger.php
@@ -4,7 +4,7 @@ Plugin Name: RefTagger
 Plugin URI: https://www.logos.com/reftagger
 Description: Transform Bible references into links to the full text of the verse.
 Author: Logos Bible Software
-Version: 2.4.1
+Version: 2.4.2
 Author URI: https://www.logos.com/
 */
 
